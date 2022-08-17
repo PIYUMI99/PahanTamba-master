6f994b4584137882c9f15d9cd153d879a5ae1da4
Piyumika <piyumikaalmeda@gmail.com>
2022-08-17 13:41:37 +0530

    First Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="INTUITIVE">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <title>Home</title>
+    <link rel="stylesheet" href="/css/index/nicepage.css" media="screen">
+    <link rel="stylesheet" href="/css/index/Home.css" media="screen">
+    <meta name="generator" content="Nicepage 4.7.1, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+    <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lobster:400">
+    
+    
+  </head>
+  <body data-home-page="Home.html" data-home-page-title="Home" class="u-body u-palette-1-base u-xl-mode">
+    <section id="carousel_84eb" class="u-carousel u-slide u-block-276a-1" data-u-ride="carousel" data-interval="5000">
+      <ol class="u-absolute-hcenter u-carousel-indicators u-block-276a-2">
+        <li data-u-target="#carousel_84eb" data-u-slide-to="0" class="u-active u-grey-30"></li>
+      </ol>
+      <div class="u-carousel-inner" role="listbox">
+        <div class="skrollable skrollable-between u-active u-align-center u-carousel-item u-clearfix u-image u-shading u-section-1-1" src="" data-image-width="1200" data-image-height="800">
+          <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+            <h1 class="u-custom-font u-font-lobster u-text u-text-1">PahanTamba<br>පහන් ටැඹ&nbsp;
+            </h1>
+            <p class="u-large-text u-text u-text-variant u-text-2"> All kids need is a little help, a little hope, and someone who believes in them</p>
+            <div class="u-clearfix u-layout-wrap u-layout-wrap-1">
+              <div class="u-layout">
+                <div class="u-layout-row">
+                  <div class="u-align-center-sm u-align-center-xs u-align-right-lg u-align-right-md u-align-right-xl u-container-style u-layout-cell u-left-cell u-size-30 u-layout-cell-1">
+                    <div class="u-container-layout u-valign-middle u-container-layout-1">
+                      <a href="/select-login-type" class="u-border-2 u-border-palette-2-base u-btn u-btn-round u-button-style u-palette-2-base u-radius-50 u-btn-1">Login</a>
+                    </div>
+                  </div>
+                  <div class="u-align-center-sm u-align-center-xs u-align-left-lg u-align-left-md u-align-left-xl u-container-style u-layout-cell u-right-cell u-size-30 u-layout-cell-2">
+                    <div class="u-container-layout u-valign-middle u-container-layout-2">
+                      <a href="/contact-us" class="u-border-2 u-border-white u-btn u-btn-round u-button-style u-radius-50 u-btn-2">Join with us</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </section>
+  </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,538 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\pagesController;
+use App\Http\Controllers\NoticeController;
+use App\Http\Controllers\GameController;
+
+
+use App\Http\Controllers\LessonController; 
+
+use App\Http\Controllers\TimetableController; 
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/blacklist', function () {
+    return view('blacklist');
+});
+
+Route::get('/view', function () {
+    return view('view');
+});
+
+Route::get('/Addfees', function () {
+    return view('Addfees');
+});
+
+
+Route::get('/Addpaymentdetails', function () {
+    return view('Addpaymentdetails');
+});
+
+Route::post('/saveSalary',[pagesController::class,'storeSalary']);
+
+Route::post('/updateSal', [pagesController::class, 'updateSal']);
+
+
+//Route::get('/view-mangesalPage', function(){
+//      $times=App\Models\Salary::all(); 
+//    return view('managesal')->with('times', $times);
+//})
+
+
+//delete salary route
+//Route::get('/deleteSal/{id}', [pagesController::class, 'deleteSal'])->name(deleteSal);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//added
+Route::get('/add-lessons', function () {
+    return view('AddLessons');
+});
+//added
+Route::get('/create-timetable', function () {
+    return view('create-timetable');
+});
+
+
+//***added */
+//insert timetable
+Route::post('/timetable-store', [TimetableController::class, 'Timetablestore']);
+
+
+//*****added */
+//manage timetable
+Route::get('/manage-timetable', function () {
+    $time=App\Models\CTimetable::all();
+    return view('manage-timetable')->with('time', $time);
+});
+
+// Route::get('/manage-timetable', function () {
+
+//     $time=App\Models\CTimetable::all();
+
+//     return view('manage-timetable')->with('time', $time);
+
+//     });
+
+
+//Route::get('/updateTimetable/{id}', [TimetableController::class,'manageTime']);
+Route::post('/updateT', [TimetableController::class,'updateshow'])->name('updateshow');
+
+Route::get('/updateT/{id}', [TimetableController::class,'manageTime']);
+
+//***added */
+//delete timetable
+Route::get('/delete/{id}', [TimetableController::class, 'delete'])->name('delete');
+
+Route::get('/common-class', function () {
+    return view('common-class');
+});
+
+
+//added
+//update lesson view pGE Route
+Route::get('/updateLessons', function () {
+    return view('Finalupdate-lessons');
+});
+
+//update - lessons 
+Route::get('/updateLessons/{id}', [LessonController::class,'updateL']);
+Route::post('/updateL', [LessonController::class,'updateshowLessons'])->name('updateshowLessons');
+
+
+//****added */
+//insert lessons
+Route::post('/Lesson-store', [LessonController::class, 'LessonStore']);
+
+
+
+//******88added */
+//edit / delete view page route 
+Route::get('/edit-lesson', function(){
+
+   $data=App\Models\Lessons::all();
+    return view('updateLessons')->with('lesson', $data);
+});
+
+
+
+//student view lessons route
+//Route::get('/view-lessons', function () {
+ //   return view('viewLessons');
+//});
+
+
+//**** Added */
+//student view-lesson common interface Route
+Route::get('/viewL-comInterface', function () {
+    return view('View-LessonCommonStudentnterface');
+});
+
+//*added
+//student view lessons route
+Route::get('/view-lessons', function(){
+
+    $data=App\Models\Lessons::all();
+     return view('viewLessons')->with('lesson', $data);
+ });
+ 
+
+ //addded
+ //view report
+ Route::get('/ClassReport-view', [LessonController::class, 'classRepo'])->name('classRepo');
+
+ Route::get('/download-classReportpdf', [LessonController::class, 'export_pdf']);
+
+ 
+ //***Added */
+  //search lessons
+  Route::post('/search-lessons',[LessonController::class, 'searchL'])->name('searchL');
+
+
+/// //report search
+Route::get('/searchStID', [LessonController::class, 'searchStID'])->name('searchStID');
+
+
+
+
+
+
+
+
+
+//**added */
+//filter lessons
+Route::get('/filter-lessons ', function () {
+    return view('FilterLessons');
+});
+
+
+//added
+  //search lessons
+  Route::post('/search-Level',[LessonController::class, 'searchLevel'])->name('searchLevel');
+
+
+//comon diseases (in order to go to view lessons)
+Route::get('/common-lessons ', function () {
+    return view('commondiseasesInterface');
+});
+
+
+
+
+
+//added
+//TimetableView Route
+
+Route::get('/view-timetable', function(){
+
+    $data=App\Models\CTimetable::all();
+     return view('TimetableView')->with('lesson', $data);
+ });
+
+//added
+        //file seen all
+        Route::get('/file', [LessonController::class, 'index'])->name('index');
+        Route::get('/view/{id}', [LessonController::class, 'show'])->name('show');
+        Route::get('/file/download/{file}', [LessonController::class, 'downloadf'])->name('downloadf');          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//****added */
+   //delete lessons
+   Route::get('/deleteL/{id}', [LessonController::class, 'delete'])->name('delete');
+
+Auth::routes([
+    'verify' => true
+]);
+
+
+
+Route::post('/saveNotice',[NoticeController::class,'storeNotice']);
+
+
+Route::post('/updateNotice1', [NoticeController::class,'updateshow']);
+
+
+//delete notice
+Route::get('/delete/{id}', [NoticeController::class, 'delete'])->name('delete');
+
+
+
+//  edit notices route
+Route::get('/edit-notice', function () {
+    return view('edit-notice');
+});
+
+
+
+
+
+
+// view-attendance route
+Route::get('/view-attendance', function () {
+    return view('view-attendance');
+});
+
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+//                         //User//
+
+// Route::prefix('user')->name('user.')->group(function(){
+  
+//     Route::middleware(['guest:web', 'PreventBackHistory'])->group(function(){
+//         Route::view('/login','dashboard.user.login')->name('login');
+//         Route::view('/register','dashboard.user.register')->name('register');
+//         Route::post('/create',[UserController::class,'create'])->name('create');
+//         Route::post('/check',[UserController::class,'check'])->name('check');
+//     });
+
+//     Route::middleware(['auth:web', 'PreventBackHistory'])->group(function(){
+//         Route::view('/home','dashboard.user.home')->name('home');
+//         Route::post('/logout',[UserController::class,'logout'])->name('logout');
+//     });
+
+// });
+
+//                     //Teacher//
+
+
+// Route::prefix('teacher')->name('teacher.')->group(function(){
+
+//     Route::middleware(['guest:teacher','PreventBackHistory'])->group(function(){
+//         Route::view('/login','dashboard.teacher.login')->name('login');
+//         Route::view('/register','dashboard.teacher.register')->name('register');
+//         Route::post('/create',[TeacherController::class,'create'])->name('create');
+//         Route::post('/check',[TeacherController::class,'check'])->name('check');
+
+//     });
+
+//     Route::middleware(['auth:teacher','PreventBackHistory'])->group(function(){
+//         Route::view('/home','dashboard.teacher.home')->name('home');
+//         Route::post('/logout',[TeacherController::class,'logout'])->name('logout');
+
+//     });
+// });
+
+
+//                         //Student//
+
+// Route::prefix('student')->name('student.')->group(function(){
+    
+//     Route::middleware(['guest:student','PreventBackHistory'])->group(function(){
+//         Route::view('/login','dashboard.student.login')->name('login');
+//         Route::view('/register','dashboard.student.register')->name('register');
+//         Route::post('/create',[StudentController::class,'create'])->name('create');
+//         Route::post('/check',[StudentController::class,'check'])->name('check'); 
+
+//     });
+
+//     Route::middleware(['auth:student','PreventBackHistory'])->group(function(){
+//         Route::view('/home','dashboard.student.home')->name('home');
+//         Route::post('/logout',[StudentController::class,'logout'])->name('logout');
+
+//     });
+// });
+
+
+Route::get('/test2', function () {
+    return view('test2');
+});
+
+Route::get('/add-task', function () {
+    return view('add-task');
+});
+
+Route::get('/add-result', function () {
+    return view('add-result');
+});
+
+Route::post('/saveTask',[TaskController::class,'storeTask']);
+
+Route::post('/saveGame',[GameController::class,'storeGame']);
+
+Route::get('view-category', function () {
+    return view('view-category');
+});
+
+Route::get('autism-level', function () {
+    return view('autism-level');
+});
+
+Route::get('down-syndrome-level', function () {
+    return view('down-syndrome-level');
+});
+
+Route::get('hyperactive-level', function () {
+    return view('hyperactive-level');
+});
+
+Route::get('manage-autism-tasks-level1', function () {
+    $data=App\Models\Task::all()->where('class', '=', 'Autism')->where('level', '=', 'Level 1');
+    return view('manage-autism-tasks-level1')->with('viewTasks',$data);
+});
+
+Route::get('manage-autism-tasks-level2', function () {
+    $data=App\Models\Task::all()->where('class', '=', 'Autism')->where('level', '=', 'Level 2');
+    return view('manage-autism-tasks-level2')->with('viewTasks',$data);
+});
+
+Route::get('manage-autism-tasks-level3', function () {
+    $data=App\Models\Task::all()->where('class', '=', 'Autism')->where('level', '=', 'Level 3');
+    return view('manage-autism-tasks-level3')->with('viewTasks',$data);
+});
+
+Route::get('manage-down-syndrome-tasks-level1', function () {
+    $data=App\Models\Task::all()->where('class', '=', 'Down syndrome')->where('level', '=', 'Level 1');
+    return view('manage-down-syndrome-tasks-level1')->with('viewTasks',$data);
+});
+
+Route::get('manage-down-syndrome-tasks-level2', function () {
+    $data=App\Models\Task::all()->where('class', '=', 'Down syndrome')->where('level', '=', 'Level 2');
+    return view('manage-down-syndrome-tasks-level2')->with('viewTasks',$data);
+});
+
+Route::get('manage-down-syndrome-tasks-level3', function () {
+    $data=App\Models\Task::all()->where('class', '=', 'Down syndrome')->where('level', '=', 'Level 3');
+    return view('manage-down-syndrome-tasks-level3')->with('viewTasks',$data);
+});
+
+Route::get('manage-hyperactive-tasks-level1', function () {
+    $data=App\Models\Task::all()->where('class', '=', 'Hyperactive')->where('level', '=', 'Level 3');
+    return view('manage-hyperactive-tasks-level1')->with('viewTasks',$data);
+});
+
+Route::get('manage-hyperactive-tasks-level2', function () {
+    $data=App\Models\Task::all()->where('class', '=', 'Hyperactive')->where('level', '=', 'Level 2');
+    return view('manage-hyperactive-tasks-level2')->with('viewTasks',$data);
+});
+
+Route::get('manage-hyperactive-tasks-level3', function () {
+    $data=App\Models\Task::all()->where('class', '=', 'Hyperactive')->where('level', '=', 'Level 3');
+    return view('manage-hyperactive-tasks-level3')->with('viewTasks',$data);
+});
+
+Route::get('select-login-type', function () {
+    return view('select-login-type');
+});
+
+Route::get('ftf-activity', function () {
+    return view('face-to-face-activities');
+});
+
+Route::post('/updateTask', [TaskController::class, 'updateTask']);
+
+Route::post('/updateGame', [GameController::class, 'updateGame']);
+
+Route::post('/saveanswer', [GameController::class, 'addanswer']);
+
+Route::post('/storeAnswer',[TaskController::class,'storeAnswer']);
+
+Route::post('/storeFinalMarks',[TaskController::class,'storeFinalMarks']);
+
+Route::get('/getStdAns', [TaskController::class, 'getStdAns'])->name('getStdAns');
+
+Route::get('/search', [TaskController::class, 'searchStdAns']);
+
+
+
+Route::get('contact-us', function () {
+    return view('contact-us');
+});
+
+Route::post('/storeAttendance', [NoticeController::class, 'storeAttendance']);
+
+
+
+
+
+// Auth::routes();
+
+
+
+
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+//                         //User//
+
+// Route::prefix('user')->name('user.')->group(function(){
+  
+//     Route::middleware(['guest:web', 'PreventBackHistory'])->group(function(){
+//         Route::view('/login','dashboard.user.login')->name('login');
+//         Route::view('/register','dashboard.user.register')->name('register');
+//         Route::post('/create',[UserController::class,'create'])->name('create');
+//         Route::post('/check',[UserController::class,'check'])->name('check');
+//     });
+
+//     Route::middleware(['auth:web', 'PreventBackHistory'])->group(function(){
+//         Route::view('/home','dashboard.user.home')->name('home');
+//         Route::post('/logout',[UserController::class,'logout'])->name('logout');
+//     });
+
+// });
+
+//                     //Teacher//
+
+
+// Route::prefix('teacher')->name('teacher.')->group(function(){
+
+//     Route::middleware(['guest:teacher','PreventBackHistory'])->group(function(){
+//         Route::view('/login','dashboard.teacher.login')->name('login');
+//         Route::view('/register','dashboard.teacher.register')->name('register');
+//         Route::post('/create',[TeacherController::class,'create'])->name('create');
+//         Route::post('/check',[TeacherController::class,'check'])->name('check');
+
+//     });
+
+//     Route::middleware(['auth:teacher','PreventBackHistory'])->group(function(){
+//         Route::view('/home','dashboard.teacher.home')->name('home');
+//         Route::post('/logout',[TeacherController::class,'logout'])->name('logout');
+
+//     });
+// });
+
+
+//                         //Student//
+
+// Route::prefix('student')->name('student.')->group(function(){
+    
+//     Route::middleware(['guest:student','PreventBackHistory'])->group(function(){
+//         Route::view('/login','dashboard.student.login')->name('login');
+//         Route::view('/register','dashboard.student.register')->name('register');
+//         Route::post('/create',[StudentController::class,'create'])->name('create');
+//         Route::post('/check',[StudentController::class,'check'])->name('check'); 
+
+//     });
+
+//     Route::middleware(['auth:student','PreventBackHistory'])->group(function(){
+//         Route::view('/home','dashboard.student.home')->name('home');
+//         Route::post('/logout',[StudentController::class,'logout'])->name('logout');
+
+//     });
+// });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
